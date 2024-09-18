b0da231638b4aa2ffb7a708fb3814f7ee6faa936
Juan Hernandez <juanc.nandez@gmail.com>
2024-09-17 20:08:21 -0500

    My First Component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,21 @@
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <div>
+      MyFirstComponent
+    </div>
+    <h1>Temas de React</h1>
+    <ul>
+        <li>Componentes</li>
+        <li>Eventos</li>
+        <li>Estados</li>
+        <li>Props</li>
+        
+    </ul>
+    </>
+  )
+}
+
+
+
